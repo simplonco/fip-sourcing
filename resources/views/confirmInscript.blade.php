d2be6c43e582b22979e7c50b67ba7b407cdb3231
raphael <moudens.raphael@orange.fr>
2017-06-13 16:58:32 +0200

    deb ut page confirm

---
+++ resources/views/confirmInscript.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<body>
+    <div class="container">
+     <section class="bord row">
+         <h2>Félicitation, vous voilà aspirant Simplonien et nous vous en remerçions !</h2>
+         <p>Vous avez reçus un e-mail automatique de confirmation dans votre boite mail</p>
+
+     </section>   
+ </div>
+</body>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/confirm', 'HomeController@confirmpage');
