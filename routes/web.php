45d64a63fe19a3650b07a5909ab118398e4a93b2
Dilou89 <odil_cadiou@orange.fr>
2017-06-13 16:33:22 +0200

    route et controller faits

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/formCivil', 'FormCivilController@index')->name('home');
