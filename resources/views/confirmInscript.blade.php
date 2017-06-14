8cb8f8cf9f5c1b01d451cbc783a947aaf5d271a1
raphael <moudens.raphael@orange.fr>
2017-06-14 10:34:22 +0200

    ajout image

--- resources/views/confirmInscript.blade.php
+++ resources/views/confirmInscript.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="shortcut icon" href="{{ asset('images/css.png') }}"> 
+<link rel="shortcut icon" href="{{ asset('images/html.png') }}"> 
+<link rel="shortcut icon" href="{{ asset('images/js.png') }}"> 
 <body>
     <div class="container">
      <section class="bord row">
          <h2>Félicitation, vous voilà aspirant Simplonien et nous vous en remerçions !</h2>
-         <p>Vous avez reçus un e-mail automatique de confirmation dans votre boite mail</p>
-
+         <p>Vous avez reçus un e-mail automatique de confirmation dans <a href="#">votre boite mail</a></p>
+         <img src="{{ asset('images/css.png') }}" alt="" class="css">
+         <img src="{{ asset('images/html.png') }}" alt="" class="html">
+         <img src="{{ asset('images/js.png') }}" alt="" class="js">
      </section>   
  </div>
 </body>
