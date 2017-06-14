b28b4e592899a0599f8640cc666340fa7552b251
JGuiraud <jerome31guiraud@gmail.com>
2017-06-14 10:38:02 +0200

    merge avec _inscription, add seeders for users, role_user, roles

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class RolesTablesSeeder extends Seeder
+class RolesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+        'lastName' => 'Admin_lastName',
+        'firstName' => 'Admin_firstName',
+        'email' => 'admin@example.com',
+        'password' => bcrypt('azerty'),
+        ]);
+
+        DB::table('users')->insert([
+        'lastName' => 'bidon',
+        'firstName' => 'jean',
+        'email' => 'bidonjean@example.com',
+        'password' => bcrypt('azerty'),
+        ]);
+
+        DB::table('users')->insert([
+        'lastName' => 'bidon',
+        'firstName' => 'paul',
+        'email' => 'bidonpaul@example.com',
+        'password' => bcrypt('azerty'),
+        ]);
+
+        DB::table('users')->insert([
+        'lastName' => 'bidon',
+        'firstName' => 'jeanette',
+        'email' => 'bidonjeanette@example.com',
+        'password' => bcrypt('azerty'),
+        ]);
+
+        DB::table('users')->insert([
+        'lastName' => 'bidon',
+        'firstName' => 'hubert',
+        'email' => 'bidonhubert@example.com',
+        'password' => bcrypt('azerty'),
+        ]);
+    }
+}
