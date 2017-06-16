cdcaa89a3c7d74f21463cff7af92979c48e9a763
JGuiraud <jerome31guiraud@gmail.com>
2017-06-16 11:00:12 +0200

    css detailed panel cote formateur

--- resources/views/candidatDetaille.blade.php
+++ resources/views/candidatDetaille.blade.php
@@ -1,4 +1,107 @@
 @extends('layouts.app') @section('content')
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+
+                <div class="panel-body">
+                <div class="panel-heading">Coordonnées</div>
+<div class="form-group">
+    <label>Nom : </label><input class="form-check-input" type="text" readonly value="{{ $details->nom->nom }}">
+</div>
+<div class="form-group">
+    <label for="">Prénom : </label><input class="form-check-input" type="text" readonly value="{{ $details->prenom->prenom }}">
+</div>
+<div class="form-group">
+    <label for="">Date de naissance : </label><input class="form-check-input" type="date" readonly value="{{ $details->date->date }}">
+</div>
+<div class="form-group">
+    <label for="">Pays : </label><input class="form-check-input" type="text" readonly value="{{ $details->pays->pays }}">
+</div>
+<div class="form-group">
+    <label for="">Nationalité : </label><input class="form-check-input" type="text" readonly value="{{ $details->national->national }}">
+</div>
+<div class="form-group">
+    <label for="">Adresse : </label>
+     <textarea class="form-control" name="Adresse" rows="10" cols="50" readonly>
+     {{ $details->adresse->adresse }}
+     </textarea>
+</div>
+<div class="form-group">
+    <label>E-mail : </label> <input class="form-check-input" type="mail" readonly value="{{ $details->email->email }}">
+</div>
+<div class="form-group">
+    <label>Numéro de téléphone : </label> <input  type="phone" class="form-check-input" readonly value="{{ $details->tel->tel }}">
+</div>
+<div class="form-group">
+    <label> Situation au moment de la candidature : </label> <input type="text" class="form-check-input" readonly value="{{ $details->statut->statut }}">
+</div>
+<div class="form-group">
+    <label>Numéro Pôle Emploi : </label><input type="text" class="form-check-input" readonly value="{{ $details->numpole->numpole }}">
+</div>
+<div class="form-group">
+    <label>Numéro de sécurité sociale : </label> <input type="text" class="form-check-input" readonly value="{{ $details->secu->secu }}">
+
+    <label>Super héros/héroïne</label><br>
+    <textarea class="form-control" name="super" rows="10" cols="50" readonly>
+     {{ $details->heros->heros }}
+    </textarea>
+
+<br>
+                <div class="panel-heading">Expériences et parcours</div>
+
+    <label>Expérience avec la programmation et/ou l'informatique</label>
+    <textarea class="form-control" name="informatique" rows="10" cols="50" readonly>
+    {{ $details->xpprog->xpprog }}
+</textarea><br><br>
+    <label>Un "hacks" technique/informatique ou pas</label><br>
+    <label>Un "hacks" technique/informatique ou pas</label><br>
+    <textarea  class="form-control" name="hacks" rows="10" cols="50" readonly>
+    {{ $details->hack->hack }}
+</textarea><br>
+    <label>Dernier diplôme obtenu : <label><input type="text" class="form-check-input" readonly value="{{ $details->etudes->etudes }}"><br>
+    <label>Le parcours</label><br>
+    <textarea class="form-control" name="parcours" rows="10" cols="50" readonly>
+    {{ $details->parcours->parcours }}
+    </textarea>
+    <label>Niveau d'anglais : </label> <input type="text" class="form-check-input" readonly value="{{ $details->anglais->anglais }}">
+
+    <br>
+
+    <div class="panel-heading">Motivations</div>
+    <label>les raisons de vouloir intégrer Simplon Occitanie</label><br>
+    <textarea class="form-control" name="raisons" rows="10" cols="50" readonly>
+    {{ $details->motivation->motivation }}
+</textarea><br><br>
+    <label>Les souhaits avec les nouveaux super-pouvoirs</label><br>
+    <textarea class="form-control" name="pouvoirs" rows="10" cols="50" readonly>
+    {{ $details->superpouvoirs->superpouvoirs }}
+</textarea><br>
+    <label>URL bagdes Codecademy : </label> <input type="url" class="form-check-input" readonly value="{{ $details->codecademy->codecademy }}">
+    <label>Profil Openclassrooms ou autre : </label> <input class="form-check-input" type="text" readonly value="{{ $details->OCR->OCR }}">
+    <label>Disponibilité pour suivre la formation suivie du stage : </label> <input class="form-check-input" type="text" readonly value="{{ $details->dispo->dispo }}"><br>
+    <label>Si non, les contraintes</label><br>
+    <textarea class="form-control" name="contraintes" rows="10" cols="50" readonly>
+    {{ $details->contraintes->contraintes }}
+</textarea><br><br>
+    <label>Financement de la vie quotidienne durant la formation</label><br>
+    <textarea class="form-control" name="financement" rows="10" cols="50" readonly>
+    {{ $details->financement->financement }}
+</textarea><br>
+    <label>Comment avez-vous entendu parler de la formation Simplon Occitanie ? </label> <input class="form-check-input" type="text" readonly value="{{ $details->notoriete->notoriete }}"><br>
+
+</div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+{{--
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -7,59 +110,6 @@
         </div>
     </div>
     <p>COORDONNEES</p>
-    Nom: <input type="text" readonly value="{{ $details->nom->nom }}">
-    <br>Prénom: <input type="text" readonly value="{{ $details->prenom->prenom }}">
-    <br>Date de naissance: <input type="date" readonly value="{{ $details->date->date }}">
-    <br>Pays: <input type="text" readonly value="{{ $details->pays->pays }}">
-    <br>Nationalité: <input type="text" readonly value="{{ $details->national->national }}"><br>
-    <span>Adresse:</span><br>
-    <textarea name="Adresse" rows="10" cols="50" readonly>
-     {{ $details->adresse->adresse }}
-</textarea><br>
-    <br>E-mail: <input type="mail" readonly value="{{ $details->email->email }}">
-    <br>Numéro de téléphone: <input type="phone" readonly value="{{ $details->tel->tel }}">
-    <br>Situation au moment de la candidature: <input type="text" readonly value="{{ $details->statut->statut }}">
-    <br>Numéro Pôle Emploi: <input type="text" readonly value="{{ $details->numpole->numpole }}">
-    <br>Numéro de sécurité sociale: <input type="text" readonly value="{{ $details->secu->secu }}"><br>
-    <span>Super héros/héroïne</span><br>
-    <textarea name="super" rows="10" cols="50" readonly>
-     {{ $details->heros->heros }}
-</textarea><br><br><br>
-    <p>EXPERIENCES ET PARCOURS</p>
-    <span>Expérience avec la programmation et/ou l'informatique</span><br>
-    <textarea name="informatique" rows="10" cols="50" readonly>
-    {{ $details->xpprog->xpprog }}
-</textarea><br><br>
-    <span>Un "hacks" technique/informatique ou pas</span><br>
-    <textarea name="hacks" rows="10" cols="50" readonly>
-    {{ $details->hack->hack }}
-</textarea><br>
-    <br>Dernier diplôme obtenu: <input type="text" readonly value="{{ $details->etudes->etudes }}"><br>
-    <span>Le parcours</span><br>
-    <textarea name="parcours" rows="10" cols="50" readonly>
-    {{ $details->parcours->parcours }}
-</textarea><br>
-    <br>Niveau d'anglais: <input type="text" readonly value="{{ $details->anglais->anglais }}"><br><br><br>
-    <p>MOTIVATIONS</p>
-    <span>les raisons de vouloir intégrer Simplon Occitanie</span><br>
-    <textarea name="raisons" rows="10" cols="50" readonly>
-    {{ $details->motivation->motivation }}
-</textarea><br><br>
-    <span>Les souhaits avec les nouveaux super-pouvoirs</span><br>
-    <textarea name="pouvoirs" rows="10" cols="50" readonly>
-    {{ $details->superpouvoirs->superpouvoirs }}
-</textarea><br>
-    <br>URL bagdes Codecademy: <input type="url" readonly value="{{ $details->codecademy->codecademy }}">
-    <br>Profil Openclassrooms ou autre: <input type="text" readonly value="{{ $details->OCR->OCR }}">
-    <br>Disponibilité pour suivre la formation suivie du stage: <input type="text" readonly value="{{ $details->dispo->dispo }}"><br>
-    <span>Si non, les contraintes</span><br>
-    <textarea name="contraintes" rows="10" cols="50" readonly>
-    {{ $details->contraintes->contraintes }}
-</textarea><br><br>
-    <span>Financement de la vie quotidienne durant la formation</span><br>
-    <textarea name="financement" rows="10" cols="50" readonly>
-    {{ $details->financement->financement }}
-</textarea><br>
-    <br>Comment avez-vous entendu parler de la formation Simplon Occitanie? <input type="text" readonly value="{{ $details->notoriete->notoriete }}"><br>
-</div>
+
+</div> --}}
 @endsection
