b8ac827a19f238d4a982490257c10dd48ab418e1
ldevernay <boblehamster@yahoo.fr>
2017-07-04 11:00:17 +0200

    formation

---
+++ app/Formation_user.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Formation_user extends Model
+{
+    //
+}
---
+++ database/migrations/2017_07_04_085134_create_formation_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormationUsersTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+      Schema::create('formation_users', function (Blueprint $table) {
+          $table->increments('id');
+          $table->integer('user_id');
+          $table->integer('formation_id');
+      });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+      Schema::dropIfExists('formation_users');
+  }
+}
