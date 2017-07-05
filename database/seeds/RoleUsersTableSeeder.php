e1e3e01d98494043d60a66acaf2178d1d9fad8df
ldevernay <boblehamster@yahoo.fr>
2017-07-05 11:37:07 +0200

    fixed seed

--- database/seeds/RoleUsersTableSeeder.php
+++ database/seeds/RoleUsersTableSeeder.php
@@ -27,14 +27,6 @@ class RoleUsersTableSeeder extends Seeder
         ]);
         DB::table('role_users')->insert([
         'user_id' => '4',
-        'role_id' => '2',
-        ]);
-        DB::table('role_users')->insert([
-        'user_id' => '5',
-        'role_id' => '2',
-        ]);
-        DB::table('role_users')->insert([
-        'user_id' => '6',
         'role_id' => '3',
         ]);
     }
