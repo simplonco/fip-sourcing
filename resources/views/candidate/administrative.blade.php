34d2f884fae909f3e6c20294b438c9639bb070cf
ldevernay <boblehamster@yahoo.fr>
2017-09-29 09:17:18 +0200

    fixed missing title for administrative form

--- resources/views/candidate/administrative.blade.php
+++ resources/views/candidate/administrative.blade.php
@@ -12,7 +12,7 @@
 
 <div class="col-sm-offset-3 col-sm-6">
   <div class="panel panel-info">
-    <div class="panel-heading"> Saisissez ici vos informations personnelles
+    <div class="panel-heading"> {{__('panel.administrative')}}
     </div>
     <div class="panel-body">
       {!! Form::model($candidate, [
