b088e178cd0b82c9e933e4aa78e77c01498655db
ravelonarivo <iravelonarivo@simplon.co>
2018-05-23 16:33:24 +0200

    updated create user page

--- resources/views/admin/createUser.blade.php
+++ resources/views/admin/createUser.blade.php
@@ -9,40 +9,44 @@
                         {{ csrf_field() }}
 
                         <div class="form-group">
-                            <label for="name" class="col-md-4 control-label">Activé</label>
+                            <label for="" class="col-md-4 control-label">Activé</label>
                             <div class="col-md-6">
                                 <input type="checkbox">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="begin_session" class="col-md-4 control-label">Rôle</label>
+                            <label for="role_id" class="col-md-4 control-label">Rôle</label>
                             <div class="col-md-6">
                                 <select name="" id="">
-                                    <option value="formateur">formateur</option>
+                                    @foreach(\App\Models\Role::all() as $role)
+                                    <option value={{$role->id}}>{{$role->name}}
+                                        @endforeach
+                                    </option>
                                 </select>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="end_session" class="col-md-4 control-label">{{ __('user.choose_user.last_name')}}</label>
                             <div class="col-md-6">
-                                <input type="text">
+                                <input type="text" name="last_name">
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="city" class="col-md-4 control-label">{{__('user.choose_user.first_name')}}</label>
                             <div class="col-md-6">
-                                <input type="text">
+                                <input type="text" name="first_name">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="city" class="col-md-4 control-label">Mail</label>
+                            <label for="city" class="col-md-4 control-label">{{ __('user.choose_user.email')}}</label>
                             <div class="col-md-6">
-                                <input type="text">
+                                <input type="mail" name="email">
                             </div>
                         </div>
 
                         <div class="container-fluid">
-                            <button class="center-block btn btn-primary" type="submit">valider</button>
+                                <a href="#" class=" btn btn-primary">RETOUR</a>
+                                <button class="center-block btn btn-primary" type="submit">ENREGISTRER</button>
                         </div>
                     </form>
                 </div>
