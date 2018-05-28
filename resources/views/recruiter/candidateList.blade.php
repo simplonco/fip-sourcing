e5787c77f4a41c397d64e686d48dacce59f969d7
Jérôme Guiraud <jerome31guiraud@gmail.com>
2018-05-28 17:06:27 +0200

    questionnaire candidat

--- resources/views/recruiter/candidateList.blade.php
+++ resources/views/recruiter/candidateList.blade.php
@@ -12,38 +12,32 @@
       <div class="title-top-container">
          <h3 class="text-center title-top">{{$session->formation->name}} du {{$session->begin_session->format('d/m/y')}} au {{$session->end_session->format('d/m/y')}} à {{$session->city}}</h3>
       </div>
-      <div class="container cards-custom-list">
-         <div class="card-custom-container-aligt-right">
-            <form class="" action="">
-               <div class="">
-                  <label for="checkbox1">
-                     <input type="checkbox"/>
-                     Afficher les candidatures en cours()
-                  </label>
-               </div>
-            </form>
-         </div>
-      </div>
-
       <div class="cards-custom-list-mega-container">
          <div class="card-custom-list-container">
             <div class="container cards-custom-list">
+                  <div class="action-buttons-container header-action-buttons">
+                     <label for="checkbox1">
+                        <input type="checkbox"/>
+                        Afficher les candidatures en cours()
+                     </label>
+                  </div>
                @include('partials.recruiter.formation', ['recruiterFormations' => 'applicantsList'])
+            <div class="action-buttons-in-footer">
+               <div class="action-button rounded-button">
+                  <a href="{{ route('recruiterIndex') }}" class="btn btn-primary"><i class="fas fa-caret-left fa-fw"></i> Retour</a>
+               </div>
+               <div class="action-button rounded-button">
+                  <a href="#{{--lien vers page de tri --}}" class="btn btn-primary"><i class="fas fa-bars fa-fw"></i> Trier</a>
+               </div>
+               <div class="action-button rounded-button">
+                  <a href="#{{-- permet de clore les candidatures pour cette session --}}" class="btn btn-primary"><i class="fas fa-times fa-fw"></i>   Clore</a>
+               </div>
             </div>
          </div>
-      </div>
-      <div class="action-buttons-container">
-            <div class="action-button rounded-button">
-                  <a href="{{ route('recruiterIndex') }}" class="btn btn-primary">Retour</a>
-            </div>
-            <div class="action-button rounded-button">
-                  <a href="#{{--lien vers page de tri --}}" class="btn btn-primary">Trier</a>
-            </div>
-            <div class="action-button rounded-button">
-                  <a href="#{{-- permet de clore les candidatures pour cette session --}}" class="btn btn-primary">Clore</a>
-            </div>
+            
          </div>
       </div>
+   </div>
 
    
 
